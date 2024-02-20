1d98c273cf327c97e13d5c6162d783a3f0ee5201
aabhiynsap <lordprabesh956@gmail.com>
2024-02-20 11:45:54 +0545

    small changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
+//import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+//
+///** @type {import('@sveltejs/kit').Config} */
+//const config = {
+//	kit: {
+//		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+//		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+//		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+//		adapter: adapter()
+//	},
+//	preprocess: vitePreprocess()
+//};
+//
+//export default config;
+
+import node from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,7 +23,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: node(),
 	},
 	preprocess: vitePreprocess()
 };
